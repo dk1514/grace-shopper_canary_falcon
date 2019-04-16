8ccbae2d67a7844b8f532e2c902948285e28cae3
markgreenquist <markgreenquist@gmail.com>
2019-04-16 16:36:30 -0400

    added action types, action creators and reducers to ./store/cart.js

---
+++ client/store/cart.js
@@ -0,0 +1,92 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const SET_CART = 'SET_CART'
+const UPDATE_CART = 'UPDATE_CART'
+const REMOVE_CART = 'REMOVE_CART'
+
+/**
+ * INITIAL STATE
+ */
+const initialState = {cart: {}}
+
+/**
+ * ACTION CREATORS
+ */
+const setCart = cart => ({type: SET_CART, cart})
+const updateCart = cart => ({type: UPDATE_CART, cart})
+const removeCart = cart => ({type: REMOVE_CART, cart})
+
+/**
+ * THUNK CREATORS
+ */
+export const setCartThunk = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/cart')
+    dispatch(setCart(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const updateCartThunk = id => async dispatch => {
+  try {
+    const res = await axios.put(`api/cart/${id}`)
+    dispatch(updateCart(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const removeCartThunk = id => async dispatch => {
+  try {
+    await axios.delete(`api/cart/${id}`)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+// export const auth = (email, password, method) => async dispatch => {
+//   let res
+//   try {
+//     res = await axios.post(`/auth/${method}`, {email, password})
+//   } catch (authError) {
+//     return dispatch(getUser({error: authError}))
+//   }
+
+//   try {
+//     dispatch(getUser(res.data))
+//     history.push('/home')
+//   } catch (dispatchOrHistoryErr) {
+//     console.error(dispatchOrHistoryErr)
+//   }
+// }
+
+// export const logout = () => async dispatch => {
+//   try {
+//     await axios.post('/auth/logout')
+//     dispatch(removeUser())
+//     history.push('/login')
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
+
+/**
+ * REDUCER
+ */
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_CART:
+      return {...state, cart: action.cart}
+    case UPDATE_CART:
+      return {...state, cart: action.cart}
+    case REMOVE_CART:
+      return {...state, cart: action.cart}
+    default:
+      return state
+  }
+}
