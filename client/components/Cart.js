db4ab4c06dba6ddeac27b48ef97a51976f6a8052
Christopher Najafi <christophernajafi@Christophers-MacBook-Air.local>
2019-04-16 15:30:50 -0400

    worked on Cart component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,17 +1,50 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+// import 'bulma/css/bulma.css'
 
 class Cart extends Component {
     constructor() {
         super()
+        this.state = {
+            quantity: 0
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            const {data} = await axios.get('/api/cart/1')
+            this.setState({quantity: data.quantity})
+        } catch(err) {console.error(err)}
     }
 
     render() {
         return (
             <div>
-                <h1>Shopping Cart</h1>
-                {/* product info */}
-                Total: $
-                <button className='button is-primary'>Checkout</button>
+                <h1 className='title'>Shopping Cart</h1>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>
+                                Product
+                            </th>
+                            <th>
+                                Image
+                            </th>
+                            <th>
+                                Quantity
+                            </th>
+                            <th>
+                                Price
+                            </th>
+                        </tr>
+                    </thead>
+                </table>
+                <div>
+                    Total: $
+                </div>
+                <div>
+                    <button className='button is-primary'>Checkout</button>
+                </div>
             </div>
         )
     }
