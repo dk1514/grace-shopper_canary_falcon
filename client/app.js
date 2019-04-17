9c074352e673026c5a952da0a623967cad51146a
Daniel Kil <dkil@Daniels-MacBook-Pro.local>
2019-04-17 15:12:02 -0400

    React router has been consolidated

--- client/app.js
+++ client/app.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import {Switch, Route} from 'react-router-dom'
 import {Navbar} from './components'
 import Routes from './routes'
-import AllHats from './components/AllHats'
-import SingleHat from './components/SingleHat'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
-      <Switch>
-        <Route exact path="/hats" component={AllHats} />
-        <Route path="/hats/:id" component={SingleHat} />
-        <Route exact path="/" component={AllHats} />
-      </Switch>
     </div>
   )
 }
