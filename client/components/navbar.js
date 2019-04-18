ad8bccfeb349010fb90eb9c19e54e4c3dc0dc374
Christopher Najafi <christophernajafi@Christophers-MacBook-Air.local>
2019-04-18 14:36:27 -0400

    working on adding items

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -10,7 +10,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+          <Link to="/">Home</Link>
           <Link to="/hats">Hats</Link>
           <Link to="/cart">Shopping Cart</Link>
           <a href="#" onClick={handleClick}>
@@ -20,7 +20,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <Link to="/home">Home</Link>
+          <Link to="/">Home</Link>
           <Link to="/hats">Hats</Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
