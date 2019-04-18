8bc283feaec8b76a281ab1b797c8238d153d44ab
Christopher Najafi <christophernajafi@Christophers-MacBook-Air.local>
2019-04-18 14:04:25 -0400

    working on adding items

---
+++ client/components/CheckoutForm.js
@@ -0,0 +1,13 @@
+// might replace success page
+
+import React, {Component} from 'react'
+
+class CheckoutForm extends Component {
+    render() {
+        return (
+            <div>Confirm order</div>
+        )
+    }
+}
+
+export default CheckoutForm
--- client/components/SingleHat.js
+++ client/components/SingleHat.js
@@ -8,7 +8,6 @@ class SingleHat extends Component {
     super()
     this.addItem = this.addItem.bind(this)
     this.handleClick = this.handleClick.bind(this)
-
   }
 
   componentDidMount() {
@@ -17,22 +16,24 @@ class SingleHat extends Component {
 
   handleClick(event) {
     event.preventDefault()
-    console.log('you added a hat!')
-    console.log('this in handleClick', this)
+    // console.log('you added a hat!')
+    // console.log('this in handleClick', this)
     this.addItem()
-    console.log('local storage after clicking', localStorage)
+    // console.log('local storage after clicking', localStorage)
     // add item to cart using localStorage
-    
   }
   
   addItem() {
-    localStorage.setItem('cart', this.props.singleHat);
+    // localStorage.setItem('cart', this.props.singleHat);
+    localStorage.setItem('cart', JSON.stringify(this.props.singleHat));
+    console.log('cart in addItem', localStorage);
+    console.log('parsed', JSON.parse(localStorage.getItem('cart')));
   }
   
   render() {
-    console.log('localStorage', localStorage)
-    console.log('this in render', this)
-    console.log('props', this.props.singleHat)
+    // console.log('localStorage', localStorage)
+    // console.log('this in render', this)
+    // console.log('props', this.props.singleHat)
     return (
       <div>
         {/* <Cart /> */}
