018b25c21230b3885342a531e3c95306f03d4af2
Christopher Najafi <christophernajafi@Christophers-MacBook-Air.local>
2019-04-18 10:17:58 -0400

    working on cart

--- client/components/EditCheckout.js
+++ client/components/EditCheckout.js
@@ -14,34 +14,47 @@ class EditCheckout extends Component {
         }
     }
 
+    componentDidMount() {
+        let localStorageCart = JSON.parse(localStorage.getItem('cart'))
+        this.setState({
+            cart: localStorageCart,
+            quantity: this.state.quantity
+          })
+    }
+
     handleChange(event) {
         this.setState({
           quantity: event.target.value
         })
       }
 
-      handleSubmit() {
+    handleSubmit() {
+        event.preventDefault()
+        if(this.props.userId) {
+            const product = this.props.product
+            const quantity = this.props.quantity
+            this.props.putQuantity
+        }
+    }
 
-      }
-
-      render() {
-          return (
-            <form onSubmit={this.handleSubmit}>
-                <li>
-                Quantity:
-                <input
-                    type="text"
-                    name="quantity"
-                    value={this.state.quantity}
-                    onChange={this.handleChange}
-                />
-                <button className="button" type="submit">
-                    Update Quantity
-                </button>
-                </li>
-            </form>
-          )
-      }
+    render() {
+        return (
+        <form onSubmit={this.handleSubmit}>
+            <li>
+            Quantity:
+            <input
+                type="text"
+                name="quantity"
+                value={this.state.quantity}
+                onChange={this.handleChange}
+            />
+            <button className="button" type="submit">
+                Update Quantity
+            </button>
+            </li>
+        </form>
+        )
+    }
 }
 
 // const mapDispatchToProps = dispatch => {
