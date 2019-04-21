2665d525366fa07f1c41e491bddbf1d45028d1ea
Christopher Najafi <christophernajafi@ChristohersiMac.fios-router.home>
2019-04-21 17:40:22 -0400

    updates

---
+++ client/store/cartReducer.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+// action types
+const ADD_TO_CART = 'ADD_TO_CART'
+const INCREASE_QUANTITY = 'INCREASE_QUANTITY'
+const DECREASE_QUANTITY = 'DECREASE_QUANTITY'
+const GET_TOTAL = 'GET_TOTAL'
+
+// action creators
+export const addToCart = (id) => ({
+  type: ADD_TO_CART,
+  id
+})
+
+export const increaseQuantity = id => ({
+  type: INCREASE_QUANITY,
+  id
+})
+
+export const decreaseQuantity = id => ({
+  type: DECREASE_QUANITY,
+  id
+})
+
+// thunks
+export const addToCartThunk = () => {
+
+}
+
+export const removeFromCartThunk = () => {
+
+}
+
+export const increaseQuantityThunk = () => {
+
+}
+
+export const decreaseQuantityThunk = () => {
+
+}
+
+
