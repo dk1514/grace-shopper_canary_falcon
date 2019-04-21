0f8bf2d1de2e44b3fca10028cbcdf8ecf561e9e4
Christopher Najafi <christophernajafi@ChristohersiMac.fios-router.home>
2019-04-21 17:06:51 -0400

    updates

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,134 +1,84 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {getCartThunk} from '../store/orders'
-import {Link} from 'react-router-dom'
-import Order from './Order'
-import {
-  getCart,
-  putCheckout,
-  postUnauthOrder,
-  deleteProductFromCart,
-  gotCart,
-  getTotal
-} from '../store'
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { removeItem,addQuantity,subtractQuantity} from '../store/cartReducer'
+import Checkout from './Checkout'
+class Cart extends Component{
 
-let defaultState = {
-  cart: {
-    products: [],
-    isCart: true
-  }
-}
-
-class Cart extends Component {
-  constructor(props) {
-    super(props)
-    this.state = defaultState
-    this.handleCheckout = this.handleCheckout.bind(this)
-    this.handleDeleteProduct = this.handleDeleteProduct.bind(this)
-  }
-
-  componentDidMount() {
-    // if (this.props.match.params.userId) {
-    //   this.props.getCart(this.props.match.params.userId)
-    // } else {
-    //   let localStorageCart = JSON.parse(localStorage.getItem('cart'))
-    //   this.setState({
-    //     cart: localStorageCart
-    //   })
-    // }
-      this.props.getCart(this.props.match.params.userId)
-  }
-
-  handleDeleteProduct(userId, productId) {
-    if (this.props.user.id) {
-      this.props.deletedProductFromCart(userId, productId)
-    } else {
-      const products = this.state.cart.products
-      const newProducts = products.filter(item => item.id !== productId)
-      this.setState({
-        cart: {
-          products: newProducts,
-          isCart: true
-        }
-      })
-      localStorage.setItem(
-        'cart',
-        JSON.stringify({products: newProducts, isCart: true})
-      )
+    // remove item from cart
+    handleRemove = (id)=>{
+        this.props.removeItem(id);
     }
-  }
-
-  handleCheckout() {
-    if (this.props.user.id) {
-      this.props.putCheckout(this.props.user.id)
-    } else {
-      let localStorageCart = JSON.parse(localStorage.getItem('cart'))
-      this.props.postUnauthOrder(localStorageCart)
-      localStorage.setItem('cart', JSON.stringify({isCart: true, products: []}))
-      let emptyCart = JSON.parse(localStorage.getItem('cart'))
-      this.setState({
-        cart: emptyCart
-      })
+    // increase quantity
+    handleAddQuantity = (id)=>{
+        this.props.addQuantity(id);
     }
-  }
-
-  render() {
-    // console.log('from cart', this.props.cart)
-    console.log('this.props', this.props)
-    let cart = {}
-    if (this.props.user.id) {
-      if (this.props.cart.products) {
-        cart = this.props.cart
-      } else {
-        cart = {isCart: true, products: []}
-      }
-    } else if (this.state.cart.products) {
-      cart = this.state.cart
-    } else {
-      cart = {isCart: true, products: []}
+    // decrease quantity
+    handleSubtractQuantity = (id)=>{
+        this.props.subtractQuantity(id);
     }
-    return (
-      <div className="cart">
-        <Order
-          user={this.props.user}
-          order={cart}
-          handleDeleteProduct={this.handleDeleteProduct}
-        />
-        {this.props.match.params.userId && cart.products.length ? (
-            <button className="button" type="submit">
-              Enter
-            </button>
-        ) : (
-          ' '
-        )}
-        {cart.products.length ? (
-            <CheckoutForm handleCheckout={this.handleCheckout} />
-        ) : (
-          <h2>Your Cart is Currently Empty</h2>
-        )}
-      </div>
-    )
-  }
+    render(){
+
+        // let addedItems = this.props.addedItems.length ?
+        //     (
+        //         this.props.addedItems.map(item=>{
+        //             return(
+
+        //                 <li className="collection-item avatar" key={item.id}>
+        //                             <div className="item-img">
+        //                                 <img src={item.img} alt={item.img} className=""/>
+        //                             </div>
+
+        //                             <div className="item-desc">
+        //                                 <span className="title">{item.title}</span>
+        //                                 <p>{item.desc}</p>
+        //                                 <p><b>Price: {item.price}$</b></p>
+        //                                 <p>
+        //                                     <b>Quantity: {item.quantity}</b>
+        //                                 </p>
+        //                                 <div className="add-remove">
+        //                                     <Link to="/cart"><i className="fas carret-up" onClick={()=>{this.handleAddQuantity(item.id)}}>arrow_drop_up</i></Link>
+        //                                     <Link to="/cart"><i className="fas caret-down" onClick={()=>{this.handleSubtractQuantity(item.id)}}>arrow_drop_down</i></Link>
+        //                                 </div>
+        //                                 <button className="button" onClick={()=>{this.handleRemove(item.id)}}>Remove</button>
+        //                             </div>
+
+        //                         </li>
+
+        //             )
+        //         })
+        //     ):
+
+            //  (
+            //     <p>Shopping cart is empty.</p>
+            //  )
+       return(
+            <div className="container">
+                <div className="cart">
+                    <h5>You have ordered:</h5>
+                    <ul className="collection">
+                        {/* {addedItems} */}
+                        ITEMS GO HERE
+                    </ul>
+                </div>
+                <Checkout />
+            </div>
+       )
+    }
 }
 
-const mapStateToProps = state => {
-  return {
-    cart: state.orders.cart,
-    user: state.user,
-    total: state.orders.total
-  }
-}
 
-const mapDispatchToProps = dispatch => {
-  return {
-    getCart: userId => dispatch(getCart(userId)),
-    putCheckout: user => dispatch(putCheckout(user)),
-    postUnauthOrder: cart => dispatch(postUnauthOrder(cart)),
-    deletedProductFromCart: (userId, productId) =>
-      dispatch(deleteProductFromCart(userId, productId)),
-    gotCart: cart => dispatch(gotCart(cart)),
-    getTotal: total => dispatch(getTotal(total))
-  }
+const mapStateToProps = (state)=>{
+    return{
+        // items: state.addedItems,
+        addedItems: state.addedItems
+    }
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(Cart)
+const mapDispatchToProps = (dispatch)=>{
+    return{
+        // removeItem: (id)=>{dispatch(removeItem(id))},
+        // addQuantity: (id)=>{dispatch(addQuantity(id))},
+        // subtractQuantity: (id)=>{dispatch(subtractQuantity(id))}
+    }
+}
+export default connect(null,null)(Cart)
